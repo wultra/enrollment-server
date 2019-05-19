dc437d3f29a518f065df918b6fd6196e6386d2e2
Petr Dvorak <petr@wultra.com>
2019-05-19 21:24:18 +0200

    Update Swagger config

--- src/main/java/com/wultra/app/enrollmentserver/controller/api/PushRegistrationController.java
+++ src/main/java/com/wultra/app/enrollmentserver/controller/api/PushRegistrationController.java
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package com.wultra.app.enrollmentserver.controller;
+package com.wultra.app.enrollmentserver.controller.api;
 
 import com.wultra.app.enrollmentserver.model.request.PushRegisterRequest;
 import com.wultra.app.enrollmentserver.errorhandling.InvalidRequestObjectException;
