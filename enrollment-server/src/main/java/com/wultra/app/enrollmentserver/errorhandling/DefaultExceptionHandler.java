e9b794c6e9daaa342aaa3a0f4d3809d0ad554a92
Roman Strobl <roman.strobl@wultra.com>
2021-12-10 12:53:39 +0100

    Fix #96: Missing JavaDoc for eciesContext in ActivationCodeController, migrate to updated restful-integration library

--- enrollment-server/src/main/java/com/wultra/app/enrollmentserver/errorhandling/DefaultExceptionHandler.java
+++ enrollment-server/src/main/java/com/wultra/app/enrollmentserver/errorhandling/DefaultExceptionHandler.java
@@ -19,7 +19,7 @@
 package com.wultra.app.enrollmentserver.errorhandling;
 
 import io.getlime.core.rest.model.base.response.ErrorResponse;
-import io.getlime.security.powerauth.rest.api.base.exception.PowerAuthAuthenticationException;
+import io.getlime.security.powerauth.rest.api.spring.exception.PowerAuthAuthenticationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
