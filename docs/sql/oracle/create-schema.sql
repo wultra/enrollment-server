adefb85f60bab9c527952b7b28df253697f44f92
Lukáš Lukovský <937429+saalistaja@users.noreply.github.com>
2022-02-01 21:09:00 +0100

    Fix oracle sql command

--- docs/sql/oracle/create-schema.sql
+++ docs/sql/oracle/create-schema.sql
@@ -144,9 +144,8 @@ CREATE TABLE ES_DOCUMENT_RESULT (
 CREATE INDEX DOCUMENT_VERIF_RESULT ON ES_DOCUMENT_RESULT (DOCUMENT_VERIFICATION_ID);
 
 -- Scheduler lock table - https://github.com/lukas-krecan/ShedLock#configure-lockprovider
-BEGIN
-EXECUTE IMMEDIATE '
-    CREATE TABLE shedlock(
+BEGIN EXECUTE IMMEDIATE '
+    CREATE TABLE shedlock2(
                                    name VARCHAR(64) NOT NULL,
                                    lock_until TIMESTAMP(3) NOT NULL,
                                    locked_at TIMESTAMP(3) NOT NULL,
@@ -155,8 +154,8 @@ EXECUTE IMMEDIATE '
     )';
 EXCEPTION
    WHEN OTHERS THEN
-      -- ignore an error if the table already exists
       IF SQLCODE != -955 THEN
          RAISE;
-END IF;
+      END IF;
 END;
+/
