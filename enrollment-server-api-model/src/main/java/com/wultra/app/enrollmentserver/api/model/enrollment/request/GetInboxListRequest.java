e2c001538bfc67bfd3664406f6a2adda875585f7
Lubos Racansky <lubos.racansky@gmail.com>
2022-12-01 15:52:21 +0100

    Fix #549: Switch jakarta.validation imports to javax.validation
    - downgrade jakarta bean validation to 2.0.2

--- enrollment-server-api-model/src/main/java/com/wultra/app/enrollmentserver/api/model/enrollment/request/GetInboxListRequest.java
+++ enrollment-server-api-model/src/main/java/com/wultra/app/enrollmentserver/api/model/enrollment/request/GetInboxListRequest.java
@@ -19,8 +19,6 @@
 package com.wultra.app.enrollmentserver.api.model.enrollment.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
--- enrollment-server-api-model/src/main/java/com/wultra/app/enrollmentserver/api/model/enrollment/request/InboxReadRequest.java
+++ enrollment-server-api-model/src/main/java/com/wultra/app/enrollmentserver/api/model/enrollment/request/InboxReadRequest.java
@@ -19,11 +19,12 @@
 package com.wultra.app.enrollmentserver.api.model.enrollment.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 /**
  * Model class for inbox message detail request.
