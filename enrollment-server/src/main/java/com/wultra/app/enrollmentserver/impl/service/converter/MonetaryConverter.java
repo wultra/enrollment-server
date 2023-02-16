fcfe02ec56cb0fb5dc5df7fe01d936ecd532ca93
Lubos Racansky <lubos.racansky@gmail.com>
2023-02-16 07:51:18 +0100

    Remove moneta dependency

--- enrollment-server/src/main/java/com/wultra/app/enrollmentserver/impl/service/converter/MonetaryConverter.java
+++ enrollment-server/src/main/java/com/wultra/app/enrollmentserver/impl/service/converter/MonetaryConverter.java
@@ -19,9 +19,6 @@ package com.wultra.app.enrollmentserver.impl.service.converter;
 
 import lombok.extern.slf4j.Slf4j;
 
-import javax.money.CurrencyUnit;
-import javax.money.Monetary;
-import javax.money.UnknownCurrencyException;
 import java.math.RoundingMode;
 import java.text.NumberFormat;
 import java.util.Currency;
@@ -82,9 +79,9 @@ class MonetaryConverter {
 
     private static int getFractionDigits(String code) {
         try {
-            final CurrencyUnit currencyUnit = Monetary.getCurrency(code);
-            return currencyUnit.getDefaultFractionDigits();
-        } catch (UnknownCurrencyException e) {
+            final Currency currency = Currency.getInstance(code);
+            return currency.getDefaultFractionDigits();
+        } catch (IllegalArgumentException e) {
             logger.debug("No currency mapping for code={}, most probably not FIAT", code);
             logger.trace("No currency mapping for code={}", code, e);
             return DEFAULT_MINIMAL_FRACTION_DIGITS;
