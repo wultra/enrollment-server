acd02bc03bcfa2dc69e389a6bc22ffb532b13518
Lubos Racansky <lubos.racansky@gmail.com>
2023-04-11 09:57:22 +0200

    Fix #704: Replace deprecated WebSecurityConfigurerAdapter

--- enrollment-server-onboarding/src/main/java/com/wultra/app/onboardingserver/configuration/SecurityConfig.java
+++ enrollment-server-onboarding/src/main/java/com/wultra/app/onboardingserver/configuration/SecurityConfig.java
@@ -18,10 +18,12 @@
 
 package com.wultra.app.onboardingserver.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 
 /**
  * Spring Security configuration.
@@ -30,12 +32,14 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
  */
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic().disable();
-        http.csrf().disable();
+    @Bean
+    public SecurityFilterChain filterChain(final HttpSecurity http) throws Exception {
+        return http
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .build();
     }
 
 }
