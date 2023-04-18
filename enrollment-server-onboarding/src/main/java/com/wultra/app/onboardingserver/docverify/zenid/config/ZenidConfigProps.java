07affa58cfac821178bdec652a8e65dfc8b00e44
roman.strobl@wultra.com <roman.strobl@wultra.com>
2023-04-18 14:49:15 +0200

    Add workaround for all Rest client initializations

--- enrollment-server-onboarding/src/main/java/com/wultra/app/onboardingserver/docverify/zenid/config/ZenidConfigProps.java
+++ enrollment-server-onboarding/src/main/java/com/wultra/app/onboardingserver/docverify/zenid/config/ZenidConfigProps.java
@@ -36,7 +36,7 @@ import java.util.Optional;
  */
 @ConditionalOnProperty(value = "enrollment-server-onboarding.document-verification.provider", havingValue = "zenid")
 @Configuration
-@ConfigurationProperties(prefix = "enrollment-server-onboarding.document-verification.zenid")
+@ConfigurationProperties(prefix = "enrollment-server-onboarding.document-verification.zenid", ignoreInvalidFields = true)
 @Getter @Setter
 public class ZenidConfigProps {
 
