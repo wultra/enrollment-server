0d332bc068536e99a883c8c7002e0f467dd60ba5
Lubos Racansky <lubos.racansky@gmail.com>
2023-11-30 14:53:34 +0100

    Remove the test call

--- enrollment-server-onboarding-provider-innovatrics/src/main/java/com/wultra/app/onboardingserver/provider/innovatrics/InnovatricsApiService.java
+++ enrollment-server-onboarding-provider-innovatrics/src/main/java/com/wultra/app/onboardingserver/provider/innovatrics/InnovatricsApiService.java
@@ -24,7 +24,6 @@ import com.wultra.app.onboardingserver.provider.innovatrics.model.api.EvaluateCu
 import com.wultra.app.onboardingserver.provider.innovatrics.model.api.EvaluateCustomerLivenessResponse;
 import com.wultra.core.rest.client.base.RestClient;
 import com.wultra.core.rest.client.base.RestClientException;
-import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -69,14 +68,6 @@ class InnovatricsApiService {
     @Autowired
     public InnovatricsApiService(@Qualifier("restClientInnovatrics") final RestClient restClient) {
         this.restClient = restClient;
-    }
-
-    // TODO remove - temporal test call
-    @PostConstruct
-    public void testCall() throws RestClientException {
-        logger.info("Trying a test call");
-        final ResponseEntity<String> response = restClient.get("/api/v1/metadata", STRING_TYPE_REFERENCE);
-        logger.info("Result of test call: {}", response.getBody());
     }
 
     public EvaluateCustomerLivenessResponse evaluateLiveness(final String customerId, final OwnerId ownerId) throws RemoteCommunicationException {
