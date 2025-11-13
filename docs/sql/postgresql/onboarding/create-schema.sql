5d4ea26c922599a51b41a3efb5edd54716ba0462
Lubos Racansky <lubos.racansky@gmail.com>
2025-11-13 09:02:27 +0100

    generate SQL

--- docs/sql/postgresql/onboarding/create-schema.sql
+++ docs/sql/postgresql/onboarding/create-schema.sql
@@ -224,3 +224,18 @@ CREATE INDEX IF NOT EXISTS audit_param_key ON audit_param (param_key);
 CREATE INDEX IF NOT EXISTS audit_param_value ON audit_param (param_value);
 
 CREATE INDEX IF NOT EXISTS onboarding_process_activation_id ON es_onboarding_process(activation_id);
+
+-- Changeset enrollment-server-onboarding/1.7.x/20250513-add-tag-1.7.0.xml::1::Lubos Racansky
+-- Changeset enrollment-server-onboarding/2.0.x/20251111-process-configuration.xml::1::Lubos Racansky
+-- Create a new sequence es_onboarding_process_configuration_seq
+CREATE SEQUENCE  IF NOT EXISTS es_onboarding_process_configuration_seq START WITH 1 INCREMENT BY 50 CACHE 20;
+
+-- Changeset enrollment-server-onboarding/2.0.x/20251111-process-configuration.xml::2::Lubos Racansky
+-- Create a new table es_onboarding_process_configuration
+CREATE TABLE es_onboarding_process_configuration (id BIGINT DEFAULT nextval('es_onboarding_process_configuration_seq') NOT NULL, process_type VARCHAR(255) NOT NULL, config TEXT NOT NULL, CONSTRAINT es_onboarding_process_configuration_pkey PRIMARY KEY (id), CONSTRAINT es_onboarding_process_configuration_process_type_uk UNIQUE (process_type));
+
+-- Changeset enrollment-server-onboarding/2.0.x/20251111-process-configuration.xml::3::Lubos Racansky
+-- Create a new column process_config_id to es_onboarding_process
+ALTER TABLE es_onboarding_process ADD process_config_id BIGINT;
+
+ALTER TABLE es_onboarding_process ADD CONSTRAINT es_onboarding_process_process_config_id_fk FOREIGN KEY (process_config_id) REFERENCES es_onboarding_process_configuration (id);
