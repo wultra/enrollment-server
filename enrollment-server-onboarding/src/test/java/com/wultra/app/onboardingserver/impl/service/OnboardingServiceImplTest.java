0aae5d8186c0a17beeb64ec00d637de8c32db6c1
Luboš Račanský <lubos.racansky@wultra.com>
2025-11-20 08:11:37 +0100

    Fix #1350: Store process type from onboarding start endpoint (#1361)
    * Fix #1350: Store process type from onboarding start endpoint

---
+++ enrollment-server-onboarding/src/test/java/com/wultra/app/onboardingserver/impl/service/OnboardingServiceImplTest.java
@@ -0,0 +1,95 @@
+/*
+ * PowerAuth Enrollment Server
+ * Copyright (C) 2025 Wultra s.r.o.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.wultra.app.onboardingserver.impl.service;
+
+import com.wultra.app.enrollmentserver.api.model.onboarding.request.OnboardingStartRequest;
+import com.wultra.app.enrollmentserver.api.model.onboarding.response.OnboardingStartResponse;
+import com.wultra.app.enrollmentserver.model.enumeration.OnboardingStatus;
+import com.wultra.app.onboardingserver.EnrollmentServerTestApplication;
+import com.wultra.app.onboardingserver.common.errorhandling.OnboardingProcessException;
+import com.wultra.core.http.common.request.RequestContext;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Test for {@link OnboardingServiceImpl}.
+ *
+ * @author Lubos Racansky, lubos.racansky@wultra.com
+ */
+@SpringBootTest(classes = EnrollmentServerTestApplication.class)
+@ActiveProfiles("test")
+@Sql
+@Transactional
+class OnboardingServiceImplTest {
+
+    @Autowired
+    private OnboardingServiceImpl tested;
+
+    @Test
+    void testStartProcess() throws Exception {
+        final OnboardingStartRequest request = OnboardingStartRequest.builder()
+                .identification(Map.of("username", "john.doe"))
+                .processType("reactivation")
+                .build();
+        final RequestContext context = RequestContext.builder().build();
+
+        final OnboardingStartResponse result = tested.startOnboarding(request, context);
+
+        assertNotNull(result);
+        assertNotNull(result.processId());
+        assertEquals(OnboardingStatus.ACTIVATION_IN_PROGRESS, result.onboardingStatus());
+        assertNull(result.activationCode());
+    }
+
+    @Test
+    void testStartProcess_defaultProcessType() throws Exception {
+        final OnboardingStartRequest request = OnboardingStartRequest.builder()
+                .identification(Map.of("username", "john.doe"))
+                .processType("") // blank on purpose
+                .build();
+        final RequestContext context = RequestContext.builder().build();
+
+        final OnboardingStartResponse result = tested.startOnboarding(request, context);
+
+        assertNotNull(result);
+        assertNotNull(result.processId());
+        assertEquals(OnboardingStatus.ACTIVATION_IN_PROGRESS, result.onboardingStatus());
+        assertNull(result.activationCode());
+    }
+
+    @Test
+    void testStartProcess_invalidProcessType() {
+        final OnboardingStartRequest request = OnboardingStartRequest.builder()
+                .identification(Map.of("username", "john.doe"))
+                .processType("non-existing")
+                .build();
+        final RequestContext context = RequestContext.builder().build();
+
+        final OnboardingProcessException result = assertThrows(OnboardingProcessException.class, () -> tested.startOnboarding(request, context));
+
+        assertEquals("No configuration found for process type: non-existing", result.getMessage());
+    }
+}
