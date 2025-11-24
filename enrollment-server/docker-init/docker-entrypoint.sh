612847dfd87133cdc70b335d89a8a7e83183b34b
Filip Bursik <filip.bursik@wultra.com>
2025-11-24 21:13:24 +0200

    Fix #1385: Update changelog file path and optimize ojdbc11 setup in enrollment-server Docker configuration

--- enrollment-server/docker-init/docker-entrypoint.sh
+++ enrollment-server/docker-init/docker-entrypoint.sh
@@ -4,7 +4,7 @@ KEEP_RUNNING=${KEEP_RUNNING:-false}
 KEEP_RUNNING_PORT=${KEEP_RUNNING_PORT:-666}
 
 liquibase --headless=true --log-level=INFO \
-    --changeLogFile="changelog/enrollment-server/db.changelog-master.xml" \
+    --changeLogFile="changelog/enrollment-server/db.changelog-module.xml" \
     --username="${ENROLLMENT_SERVER_DATASOURCE_USERNAME:-}" \
     --password="${ENROLLMENT_SERVER_DATASOURCE_PASSWORD:-}" \
     --url="${ENROLLMENT_SERVER_DATASOURCE_URL}" \
