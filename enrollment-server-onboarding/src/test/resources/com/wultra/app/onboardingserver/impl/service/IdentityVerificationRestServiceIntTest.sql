a400590de795ed1d03f9e63cfacec6706dd1734c
Lubos Racansky <lubos.racansky@gmail.com>
2025-12-12 09:44:23 +0100

    revert sql changes

--- enrollment-server-onboarding/src/test/resources/com/wultra/app/onboardingserver/impl/service/IdentityVerificationRestServiceIntTest.sql
+++ enrollment-server-onboarding/src/test/resources/com/wultra/app/onboardingserver/impl/service/IdentityVerificationRestServiceIntTest.sql
@@ -1,16 +1,13 @@
-insert into es_onboarding_process_configuration (id, process_type, config) values
-    (1, 'onboarding', '{}');
-
 -- test testSubmitDocumentsV2_newDocumentsAreSubmitted_responseOk
-insert into es_onboarding_process (id, process_config_id, identification_data, activation_id, status, custom_data, timestamp_created, error_score)
-values ( 'b9d4cf32-3e3c-4bb1-8f66-5a3c91fe2f8b', 1, '{}', 'b7717831-4ed3-4597-88c1-b4646b91a76f', 'VERIFICATION_IN_PROGRESS', '{}', now(), 0);
+insert into es_onboarding_process (id, identification_data, activation_id, status, custom_data, timestamp_created, error_score)
+values ( 'b9d4cf32-3e3c-4bb1-8f66-5a3c91fe2f8b', '{}', 'b7717831-4ed3-4597-88c1-b4646b91a76f', 'VERIFICATION_IN_PROGRESS', '{}', now(), 0);
 
 insert into es_identity_verification (id, activation_id, user_id, process_id, status, phase, timestamp_created)
 values ('e0e8d44b-f51e-4cdf-9bc6-3cc2c0850b01', 'b7717831-4ed3-4597-88c1-b4646b91a76f', 'test-user', 'b9d4cf32-3e3c-4bb1-8f66-5a3c91fe2f8b', 'IN_PROGRESS', 'DOCUMENT_UPLOAD', now());
 
 -- test testSubmitDocumentsV2_documentsAreResubmitted_responseOk
-insert into es_onboarding_process (id, process_config_id, identification_data, activation_id, status, custom_data, timestamp_created, error_score)
-values ( 'c3c2c4c1-2a84-4c6e-a8bd-4e3b824d5c91', 1, '{}', '8e4a0b0a-84f3-4c71-9cc0-c9ac7b3f6593', 'VERIFICATION_IN_PROGRESS', '{}', now(), 0);
+insert into es_onboarding_process (id, identification_data, activation_id, status, custom_data, timestamp_created, error_score)
+values ( 'c3c2c4c1-2a84-4c6e-a8bd-4e3b824d5c91', '{}', '8e4a0b0a-84f3-4c71-9cc0-c9ac7b3f6593', 'VERIFICATION_IN_PROGRESS', '{}', now(), 0);
 
 insert into es_identity_verification (id, activation_id, user_id, process_id, status, phase, timestamp_created)
 values ('1f9f3d7c-5c41-4f62-8c18-97af2df6bd62', '8e4a0b0a-84f3-4c71-9cc0-c9ac7b3f6593', 'test-user-2', 'c3c2c4c1-2a84-4c6e-a8bd-4e3b824d5c91', 'IN_PROGRESS', 'DOCUMENT_UPLOAD', now());
