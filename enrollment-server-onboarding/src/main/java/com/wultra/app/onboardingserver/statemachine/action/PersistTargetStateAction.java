f7f12420713206e96940c6f77412e284b0a61cf5
Lubos Racansky <lubos.racansky@gmail.com>
2026-01-22 10:29:41 +0100

    PersistTargetStateAction

---
+++ enrollment-server-onboarding/src/main/java/com/wultra/app/onboardingserver/statemachine/action/PersistTargetStateAction.java
@@ -0,0 +1,55 @@
+/*
+ * PowerAuth Enrollment Server
+ * Copyright (C) 2026 Wultra s.r.o.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.wultra.app.onboardingserver.statemachine.action;
+
+import com.wultra.app.enrollmentserver.model.integration.OwnerId;
+import com.wultra.app.onboardingserver.common.database.entity.IdentityVerificationEntity;
+import com.wultra.app.onboardingserver.impl.service.IdentityVerificationService;
+import com.wultra.app.onboardingserver.statemachine.consts.EventHeaderName;
+import com.wultra.app.onboardingserver.statemachine.consts.ExtendedStateVariable;
+import com.wultra.app.onboardingserver.statemachine.enums.OnboardingEvent;
+import com.wultra.app.onboardingserver.statemachine.enums.OnboardingState;
+import lombok.AllArgsConstructor;
+import org.springframework.statemachine.StateContext;
+import org.springframework.statemachine.action.Action;
+import org.springframework.stereotype.Component;
+
+/**
+ * Action persisting the target state.
+ *
+ * @author Lubos Racansky, lubos.racansky@wultra.com
+ */
+@Component
+@AllArgsConstructor
+public class PersistTargetStateAction implements Action<OnboardingState, OnboardingEvent> {
+
+    private final IdentityVerificationService identityVerificationService;
+
+    @Override
+    public void execute(final StateContext<OnboardingState, OnboardingEvent> context) {
+        final OnboardingState targetState = context.getTarget().getId();
+
+        if (targetState.isChoiceState()) {
+            throw new IllegalStateException("Cannot persist identity verification for a choice state: " + targetState);
+        }
+
+        final OwnerId ownerId = (OwnerId) context.getMessageHeader(EventHeaderName.OWNER_ID);
+        final IdentityVerificationEntity identityVerification = context.getExtendedState().get(ExtendedStateVariable.IDENTITY_VERIFICATION, IdentityVerificationEntity.class);
+
+        identityVerificationService.moveToPhaseAndStatus(identityVerification, targetState.getPhase(), targetState.getStatus(), ownerId);
+    }
+}
