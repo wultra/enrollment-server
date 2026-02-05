d6da79dbcbeb931b045b4a85a9932e56815f8ba1
Luboš Račanský <lubos.racansky@wultra.com>
2026-02-05 15:31:20 +0100

    Fix #1503: Enrollment Server Onboarding doesn't have script for audit (#1509)

--- docs/sql/postgresql/onboarding/migration_1.10.0_2.0.0.sql
+++ docs/sql/postgresql/onboarding/migration_1.10.0_2.0.0.sql
@@ -31,3 +31,43 @@ CREATE INDEX es_selfie_identity_verification_id_idx ON es_selfie(identity_verifi
 -- Changeset enrollment-server-onboarding/2.0.x/20260126-selfie.xml::4::Lubos Racansky
 -- Create a new index es_selfie_timestamp_created_idx
 CREATE INDEX es_selfie_timestamp_created_idx ON es_selfie(timestamp_created);
+
+-- Changeset enrollment-server/2.0.x/20260205-audit.xml::1::Lubos Racansky
+-- Create a new table audit_log
+CREATE TABLE audit_log (audit_log_id VARCHAR(36) NOT NULL, application_name VARCHAR(256) NOT NULL, audit_level VARCHAR(32) NOT NULL, audit_type VARCHAR(256), timestamp_created TIMESTAMP WITHOUT TIME ZONE DEFAULT NOW(), message TEXT NOT NULL, exception_message TEXT, stack_trace TEXT, param TEXT, calling_class VARCHAR(256) NOT NULL, thread_name VARCHAR(256) NOT NULL, version VARCHAR(256), build_time TIMESTAMP WITHOUT TIME ZONE, CONSTRAINT audit_log_pkey PRIMARY KEY (audit_log_id));
+
+-- Changeset enrollment-server/2.0.x/20260205-audit.xml::2::Lubos Racansky
+-- Create a new table audit_log
+CREATE TABLE audit_param (audit_log_id VARCHAR(36), timestamp_created TIMESTAMP WITHOUT TIME ZONE DEFAULT NOW(), param_key VARCHAR(256), param_value VARCHAR(4000));
+
+-- Changeset enrollment-server/2.0.x/20260205-audit.xml::3::Lubos Racansky
+-- Create a new index on audit_log(timestamp_created)
+CREATE INDEX audit_log_timestamp ON audit_log(timestamp_created);
+
+-- Changeset enrollment-server/2.0.x/20260205-audit.xml::4::Lubos Racansky
+-- Create a new index on audit_log(application_name)
+CREATE INDEX audit_log_application ON audit_log(application_name);
+
+-- Changeset enrollment-server/2.0.x/20260205-audit.xml::5::Lubos Racansky
+-- Create a new index on audit_log(audit_level)
+CREATE INDEX audit_log_level ON audit_log(audit_level);
+
+-- Changeset enrollment-server/2.0.x/20260205-audit.xml::6::Lubos Racansky
+-- Create a new index on audit_log(audit_type)
+CREATE INDEX audit_log_type ON audit_log(audit_type);
+
+-- Changeset enrollment-server/2.0.x/20260205-audit.xml::7::Lubos Racansky
+-- Create a new index on audit_param(audit_log_id)
+CREATE INDEX audit_param_log ON audit_param(audit_log_id);
+
+-- Changeset enrollment-server/2.0.x/20260205-audit.xml::8::Lubos Racansky
+-- Create a new index on audit_param(timestamp_created)
+CREATE INDEX audit_param_timestamp ON audit_param(timestamp_created);
+
+-- Changeset enrollment-server/2.0.x/20260205-audit.xml::9::Lubos Racansky
+-- Create a new index on audit_log(param_key)
+CREATE INDEX audit_param_key ON audit_param(param_key);
+
+-- Changeset enrollment-server/2.0.x/20260205-audit.xml::10::Lubos Racansky
+-- Create a new index on audit_log(param_value)
+CREATE INDEX audit_param_value ON audit_param(param_value);
