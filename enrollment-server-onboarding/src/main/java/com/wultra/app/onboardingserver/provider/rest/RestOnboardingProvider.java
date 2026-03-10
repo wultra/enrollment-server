1b684bc12f452868e8f50d3a0189c5587cf5b5d7
michal-rozehnal-w <michal.rozehnal@wultra.com>
2026-03-10 09:40:53 +0100

    Add document images into evaluation request

--- enrollment-server-onboarding/src/main/java/com/wultra/app/onboardingserver/provider/rest/RestOnboardingProvider.java
+++ enrollment-server-onboarding/src/main/java/com/wultra/app/onboardingserver/provider/rest/RestOnboardingProvider.java
@@ -411,7 +411,8 @@ public class RestOnboardingProvider implements OnboardingProvider {
     private static ClientEvaluateRequestDto.ImageType convert(final ProcessedDocumentDataType source) {
         return switch (source) {
             case FACE_IMAGE -> ClientEvaluateRequestDto.ImageType.FACE;
-            default -> throw new IllegalArgumentException("Unsupported image type: " + source);
+            case DOCUMENT_FRONT_SIDE -> ClientEvaluateRequestDto.ImageType.DOCUMENT_FRONT_SIDE;
+            case DOCUMENT_BACK_SIDE -> ClientEvaluateRequestDto.ImageType.DOCUMENT_BACK_SIDE;
         };
     }
 }
