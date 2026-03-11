d56615d3fe0d0c1279f294ca0f7f74b0beb9ad24
michal-rozehnal-w <michal.rozehnal@wultra.com>
2026-03-11 13:15:00 +0100

    Update sql scripts

--- docs/sql/oracle/onboarding/migration_1.10.0_2.0.0.sql
+++ docs/sql/oracle/onboarding/migration_1.10.0_2.0.0.sql
@@ -12,9 +12,37 @@ ALTER TABLE es_onboarding_process ADD process_config_id NUMBER(38, 0);
 
 ALTER TABLE es_onboarding_process ADD CONSTRAINT es_onboarding_process_process_config_id_fk FOREIGN KEY (process_config_id) REFERENCES es_onboarding_process_configuration (id);
 
--- Changeset enrollment-server-onboarding/2.1.x/20251219-process-target-activation-id.xml::1::Lubos Racansky
+-- Changeset enrollment-server-onboarding/2.0.x/20251219-process-target-activation-id.xml::1::Lubos Racansky
 -- Create a new column target_activation_id to es_onboarding_process
 ALTER TABLE es_onboarding_process ADD target_activation_id VARCHAR2(36);
+
+-- Changeset enrollment-server-onboarding/2.0.x/20260116-add-processed-document-data-table.xml::1::Michal Rozehnal
+-- Create a new table es_processed_document_data
+CREATE TABLE es_processed_document_data (id VARCHAR2(36) NOT NULL, data BLOB NOT NULL, data_type VARCHAR2(32) NOT NULL, timestamp_created TIMESTAMP DEFAULT sysdate NOT NULL, CONSTRAINT PK_ES_PROCESSED_DOCUMENT_DATA PRIMARY KEY (id));
+
+-- Changeset enrollment-server-onboarding/2.0.x/20260116-add-processed-document-data-table.xml::2::Michal Rozehnal
+-- Remove column activation_id from es_document_data
+ALTER TABLE es_document_data DROP COLUMN activation_id;
+
+-- Changeset enrollment-server-onboarding/2.0.x/20260116-add-processed-document-data-table.xml::3::Michal Rozehnal
+-- Remove foreign key es_document_data_identity_verification_id_fk
+ALTER TABLE es_document_data DROP CONSTRAINT es_document_data_identity_verification_id_fk;
+
+-- Changeset enrollment-server-onboarding/2.0.x/20260116-add-processed-document-data-table.xml::4::Michal Rozehnal
+-- Remove column identity_verification_id from es_document_data
+ALTER TABLE es_document_data DROP COLUMN identity_verification_id;
+
+-- Changeset enrollment-server-onboarding/2.0.x/20260116-add-processed-document-data-table.xml::5::Michal Rozehnal
+-- Remove column filename from es_document_data
+ALTER TABLE es_document_data DROP COLUMN filename;
+
+-- Changeset enrollment-server-onboarding/2.0.x/20260116-add-processed-document-data-table.xml::6::Michal Rozehnal
+-- Add index for table es_document_data column timestamp_created
+CREATE INDEX es_document_data_timestamp_created_idx ON es_document_data(timestamp_created);
+
+-- Changeset enrollment-server-onboarding/2.0.x/20260116-add-processed-document-data-table.xml::7::Michal Rozehnal
+-- Add index for table es_document_verification column upload_id
+CREATE INDEX es_document_verification_upload_id_idx ON es_document_verification(upload_id);
 
 -- Changeset enrollment-server-onboarding/2.0.x/20260126-selfie.xml::1::Lubos Racansky
 -- Create a new sequence es_selfie_seq
@@ -71,3 +99,15 @@ CREATE INDEX audit_param_key ON audit_param(param_key);
 -- Changeset enrollment-server/2.0.x/20260205-audit.xml::10::Lubos Racansky
 -- Create a new index on audit_log(param_value)
 CREATE INDEX audit_param_value ON audit_param(param_value);
+
+-- Changeset enrollment-server-onboarding/2.0.x/20260309-processed-document-data-verification-id.xml::1::Michal Rozehnal
+-- Creates a new column document_verification_id in es_processed_document_data
+ALTER TABLE es_processed_document_data ADD document_verification_id VARCHAR2(36);
+
+ALTER TABLE es_processed_document_data ADD CONSTRAINT es_processed_document_data_document_verification_id_fk FOREIGN KEY (document_verification_id) REFERENCES es_document_verification (id);
+
+-- Changeset enrollment-server-onboarding/2.0.x/20260309-processed-document-data-verification-id.xml::2::Michal Rozehnal
+-- Creates a new index es_processed_document_data_document_verification_id_idx
+CREATE INDEX es_processed_document_data_document_verification_id_idx ON es_processed_document_data(document_verification_id);
+
+-- Changeset enrollment-server-onboarding/2.0.x/20251215-add-tag-2.0.0.xml::1::Lubos Racansky
