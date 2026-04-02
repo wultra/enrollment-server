4d39b31bf5aa6d9b185504571e3219b0746258ad
Luboš Račanský <lubos.racansky@wultra.com>
2026-04-02 10:33:53 +0200

    Update enrollment-server-onboarding-common/src/main/java/com/wultra/app/onboardingserver/common/database/entity/DocumentVerificationEntity.java

--- enrollment-server-onboarding-common/src/main/java/com/wultra/app/onboardingserver/common/database/entity/DocumentVerificationEntity.java
+++ enrollment-server-onboarding-common/src/main/java/com/wultra/app/onboardingserver/common/database/entity/DocumentVerificationEntity.java
@@ -89,7 +89,7 @@ public class DocumentVerificationEntity implements Serializable {
     private String otherSideId;
 
     /**
-     * Country stored as an ISO 3166-1 alpha-3 code, e.g. CZE.
+     * Country stored as an ISO 3166-1 alpha-3 code, e.g. {@code CZE}.
      */
     @Column(name = "country", length = 3)
     private String country;
