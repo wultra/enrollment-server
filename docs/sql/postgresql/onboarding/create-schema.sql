4ced6bfaab741e975dc4ed9151aebf196b4f4e4d
michal-rozehnal-w <michal.rozehnal@wultra.com>
2026-04-10 09:33:24 +0200

    Update SQL scripts

--- docs/sql/postgresql/onboarding/create-schema.sql
+++ docs/sql/postgresql/onboarding/create-schema.sql
@@ -355,3 +355,25 @@ CREATE INDEX audit_log_subject_id_idx ON audit_log(subject_id);
 -- Changeset enrollment-server-onboarding/2.0.x/20260402-document-country.xml::1::Lubos Racansky
 -- Creates a new column country in es_document_verification
 ALTER TABLE es_document_verification ADD country VARCHAR(3);
+
+-- Changeset docs/db/changelog/changesets/enrollment-server-onboarding/2.1.x/20260408-personal-data-cleaned-timestamp.xml::1::Michal Rozehnal
+-- Create a new column timestamp_personal_data_cleaned in es_onboarding_process table to store the timestamp when the identity data was cleaned
+ALTER TABLE es_onboarding_process ADD timestamp_personal_data_cleaned TIMESTAMP WITHOUT TIME ZONE;
+
+-- Changeset docs/db/changelog/changesets/enrollment-server-onboarding/2.1.x/20260408-personal-data-cleaned-timestamp.xml::2::Michal Rozehnal
+-- Create a new index es_onboarding_process_timestamp_personal_data_cleaned_idx
+CREATE INDEX es_onboarding_process_timestamp_personal_data_cleaned_idx ON es_onboarding_process(timestamp_personal_data_cleaned);
+
+-- Changeset docs/db/changelog/changesets/enrollment-server-onboarding/2.1.x/20260408-personal-data-cleaned-timestamp.xml::3::Michal Rozehnal
+-- Create a new index es_identity_verification_process_id_idx
+CREATE INDEX es_identity_verification_process_id_idx ON es_identity_verification(process_id);
+
+-- Changeset docs/db/changelog/changesets/enrollment-server-onboarding/2.1.x/20260408-personal-data-cleaned-timestamp.xml::4::Michal Rozehnal
+-- Create a new column document_verification_id in es_document_data table
+ALTER TABLE es_document_data ADD document_verification_id VARCHAR(36);
+
+ALTER TABLE es_document_data ADD CONSTRAINT es_document_verification_id_fk FOREIGN KEY (document_verification_id) REFERENCES es_document_verification (id);
+
+-- Changeset docs/db/changelog/changesets/enrollment-server-onboarding/2.1.x/20260408-personal-data-cleaned-timestamp.xml::5::Michal Rozehnal
+-- Create a new index es_document_data_document_verification_id_idx
+CREATE INDEX es_document_data_document_verification_id_idx ON es_document_data(document_verification_id);
