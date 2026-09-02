83b73dbf79f41a65fabacf399f5d8e737ffb02f0
Lubos Racansky <lubos.racansky@gmail.com>
2026-09-02 09:22:00 +0200

    change message

--- enrollment-server-onboarding-provider-microblink/src/main/java/com/wultra/app/onboardingserver/provider/microblink/MicroblinkDocumentVerificationProvider.java
+++ enrollment-server-onboarding-provider-microblink/src/main/java/com/wultra/app/onboardingserver/provider/microblink/MicroblinkDocumentVerificationProvider.java
@@ -65,7 +65,7 @@ public class MicroblinkDocumentVerificationProvider implements DocumentVerificat
 
     private static final Pattern MICROBLINK_TRACE_ID_PATTERN = Pattern.compile("\"traceId\"\\s*:\\s*\"([^\"]+)\"");
     private static final String MICROBLINK_VALIDATION_PASS_RESULT = "Pass";
-    private static final String MICROBLINK_VALIDATION_FAILED_REASON = "Document validation failed — no details from Microblink.";
+    private static final String MICROBLINK_VALIDATION_FAILED_REASON = "Other";
 
     private final RestClient microblinkRestClient;
     private final ObjectMapper objectMapper;
